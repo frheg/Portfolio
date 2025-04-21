6b8bfb731e13d01cd8f173d339db3081c410c426
Fredric Hegland <114155995+frheg@users.noreply.github.com>
2025-04-21 15:26:17 +0200

    Added more context

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@
 // =========================
 
 import './style.css';
-import * as THREE from "../node_modules/three/build/three.module.js";
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "three";
+// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // =========================
 // Constants & Scene Setup
